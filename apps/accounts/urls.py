bfba5a13353f673a136b5cee13253c2b9b173c1e
clement <clemcy9@gmail.com>
2026-01-16 12:26:13 +0100

    feat: added url for otp verification

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OTPRequestView
+from .views import OTPRequestView, OTPVerifyView
 
 urlpatterns = [
     path("auth/otp/request", OTPRequestView.as_view(), name="otp_request"),
+    path("auth/otp/verify", OTPVerifyView.as_view(), name="otp_verify"),
 ]
